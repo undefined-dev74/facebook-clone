70587b97bfa37b3f21d18baaff0922291c791330
JuniorBaymax <cooladitya953@gmail.com>
2021-01-03 00:26:56 +0530

    MessageSender and feed component added, adding style after this commit

--- src/Feed.js
+++ src/Feed.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Feed.css';
+import MessageSender from './MessageSender';
+import Post from './Post';
 import StoryReel from './StoryReel';
+
 function Feed() {
   return (
     <div className="feed">
-      {/* TODO StoryRow */}
       <StoryReel />
-      {/* TODO MessageSender */}
+      <MessageSender />
+      <Post
+        profilePic={
+          'https://avatars0.githubusercontent.com/u/45736218?s=460&u=e1b306e04f04b1c0d973283e7039b72e1de754cd&v=4'
+        }
+        message={'The first post message'}
+        timestamp="8am"
+        image="https://images.unsplash.com/photo-1564865878688-9a244444042a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+        username="aditya._jha"
+      />
+      <Post />
+      <Post />
     </div>
   );
 }
---
+++ src/MessageSender.js
@@ -0,0 +1,57 @@
+import { Avatar } from '@material-ui/core';
+import { InsertEmoticon, PhotoLibrary, Videocam } from '@material-ui/icons';
+import React, { useState } from 'react';
+import './MessageSender.css';
+
+const MessageSender = () => {
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setInput('');
+    setImageUrl('');
+  };
+
+  return (
+    <div className="messageSender">
+      <div className="messageSender__top">
+        <form>
+          <Avatar />
+          <input
+            value={input}
+            onChange={e => setInput(e.target.value)}
+            className="messageSender__input"
+            type="text"
+            placeholder={`what's in your mind?`}
+          />
+          <input
+            value={imageUrl}
+            onChange={e => setImageUrl(e.target.value)}
+            placeholder="image URL (Optional)"
+            type="text"
+          />
+          <button onClick={handleSubmit} type="submit">
+            Hidden Submit
+          </button>
+        </form>
+      </div>
+      <div className="messageSender__bottom">
+        <div className="messageSender__option">
+          <Videocam style={{ color: ' red' }} />
+          <h3>Live Video</h3>
+        </div>
+        <div className="messageSender__option">
+          <PhotoLibrary style={{ color: 'green' }} />
+          <h3>Photo/Video</h3>
+        </div>
+        <div className="messageSender__option">
+          <InsertEmoticon style={{ color: 'yellow' }} />
+          <h3>Feeling/Activity</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MessageSender;
